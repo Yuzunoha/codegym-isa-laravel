0bdde545c33cc8b7515626e5a4ed8668154b5f1c
Yuzunoha <studysteadystandby@gmail.com>
2021-01-21 21:47:06 +0900

    yieldは親。sectionは子ども。親のsectionは子から上書き可能である

--- html/laravelapp/resources/views/hello/index.blade.php
+++ html/laravelapp/resources/views/hello/index.blade.php
@@ -1,35 +1,17 @@
-<html>
+@extends('layouts.helloapp')
 
-<head>
-  <title>Hello/Index</title>
-  <style>
-    body {
-      font-size: 16pt;
-      color: #999;
-    }
+@section('title', 'Index')
 
-    h1 {
-      font-size: 50pt;
-      text-align: right;
-      color: #f6f6f6;
-      margin: -20px 0px -30px 0px;
-      letter-spacing: -4pt;
-    }
-  </style>
-</head>
+@section('menubar')
+   @parent
+   インデックスページ
+@endsection
 
-<body>
-  <p>whileディレクティブとphpディレクティブの例2</p>
-  @php
-    $cnt = 0;
-    $length = count($data);
-  @endphp
+@section('content')
+   <p>ここが本文のコンテンツです。</p>
+   <p>必要なだけ記述できます。</p>
+@endsection
 
-  <ol>
-  @while ($cnt < $length)
-    <li>{{$data[$cnt++]}}</li>
-  @endwhile
-  </ol>
-</body>
-
-</html>
+@section('footer')
+copyright 2017 tuyano.
+@endsection
