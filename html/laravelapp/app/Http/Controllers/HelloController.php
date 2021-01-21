65d32fc1b9350f89dd735140ac0878e8ad16fa61
Yuzunoha <studysteadystandby@gmail.com>
2021-01-21 15:30:24 +0900

    csrf対策ｄ

--- html/laravelapp/app/Http/Controllers/HelloController.php
+++ html/laravelapp/app/Http/Controllers/HelloController.php
@@ -9,8 +9,14 @@ class HelloController extends Controller
 {
     function index()
     {
-        $data = ['msg' => 'これはBladeを利用したサンプルです!!',];
+        $data = ['msg' => 'お名前を入力してください。',];
+        return view('hello.index', $data);
+    }
 
+    function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = ['msg' => "こんにちは。${msg}さん。",];
         return view('hello.index', $data);
     }
 }
