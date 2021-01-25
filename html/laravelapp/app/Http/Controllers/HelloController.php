e1f10fdc7af5611673bfc4151859c11f2c457c3b
Yuzunoha <studysteadystandby@gmail.com>
2021-01-25 17:25:12 +0900

    クッキー

--- html/laravelapp/app/Http/Controllers/HelloController.php
+++ html/laravelapp/app/Http/Controllers/HelloController.php
@@ -2,20 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('hello.index', ['msg' => 'フォームを入力：']);
+        if ($request->hasCookie('msg')) {
+            $msg = 'Cookie: ' . $request->cookie('msg');
+        } else {
+            $msg = '※クッキーはありません。';
+        }
+        $view = view('hello.index', [
+            'msg' => $msg
+        ]);
+        return $view;
     }
 
     public function post(Request $request)
     {
-        return view('hello.index', ['msg' => '正しく入力されました！']);
+        $validate_rule = ['msg' => 'required',];
+        $this->validate($request, $validate_rule);
+
+        $msg = $request->msg;
+        $response = new Response(view('hello.index', [
+            'msg' => '「' . $msg . '」をクッキーに保存しました。'
+        ]));
+        $response->cookie('msg', $msg, 100);
+        return  $response;
     }
 }
