680b63aa3cdc4f68d59f344d963fea5a0f8047a6
Yuzunoha <studysteadystandby@gmail.com>
2021-01-26 17:49:44 +0900

    dbから値を取得ｄ

--- html/laravelapp/app/Http/Controllers/HelloController.php
+++ html/laravelapp/app/Http/Controllers/HelloController.php
@@ -4,32 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        $view = view('hello.index', [
-            'msg' => $msg
-        ]);
+        $sql = 'select * from people';
+        $items = DB::select($sql);
+        $view = view('hello.index', ['items' => $items]);
         return $view;
-    }
-
-    public function post(Request $request)
-    {
-        $validate_rule = ['msg' => 'required',];
-        $this->validate($request, $validate_rule);
-
-        $msg = $request->msg;
-        $response = new Response(view('hello.index', [
-            'msg' => '「' . $msg . '」をクッキーに保存しました。'
-        ]));
-        $response->cookie('msg', $msg, 100);
-        return  $response;
     }
 }
