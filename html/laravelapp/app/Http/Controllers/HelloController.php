d9b8f3ee8ec5687d95afc7f97a7caa244c2ea696
Yuzunoha <studysteadystandby@gmail.com>
2021-02-04 12:49:51 +0900

    さんぷるこーど

--- html/laravelapp/app/Http/Controllers/HelloController.php
+++ html/laravelapp/app/Http/Controllers/HelloController.php
@@ -14,6 +14,12 @@ class HelloController extends Controller
         return view('hello.index', ['items' => $items]);
     }
 
+    public function post(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
     public function add(Request $request)
     {
         return view('hello.add');
@@ -21,6 +27,12 @@ class HelloController extends Controller
 
     public function create(Request $request)
     {
-        return view('hello.add');
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
     }
 }
