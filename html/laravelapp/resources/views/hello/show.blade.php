71f82d8d3be8ef15d9734b8ea0e7040fe527638b
Yuzunoha <studysteadystandby@gmail.com>
2021-02-04 16:06:04 +0900

    nameとmailであいまいor検索

--- html/laravelapp/resources/views/hello/show.blade.php
+++ html/laravelapp/resources/views/hello/show.blade.php
@@ -10,11 +10,15 @@
 @section('content')
    @if ($items != null)
        @foreach($items as $item)
-       <table width="400px">
-       <tr><th width="50px">id:</th>
+       <table>
+       <tr>
+       <th width="50px">id:</th>
        <td width="50px">{{$item->id}}</td>
        <th width="50px">name:</th>
-       <td>{{$item->name}}</td></tr>
+       <td width="200px">{{$item->name}}</td>
+       <th width="50px">mail:</th>
+       <td width="300px">{{$item->mail}}</td>
+       </tr>
        </table>
        @endforeach
    @endif
