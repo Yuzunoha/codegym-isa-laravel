5c5361ffa319a85f2e991cdc9c426d083f027f49
Yuzunoha <studysteadystandby@gmail.com>
2021-02-17 01:34:12 +0900

    サンプルコード。ただしクエリパラメタなしだとエラーが出る

--- html/laravelapp/app/Http/Controllers/HelloController.php
+++ html/laravelapp/app/Http/Controllers/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Person;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
@@ -10,8 +11,10 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = Db::table('people')->simplePaginate(5);
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        $items = Person::orderBy($sort, 'asc')->simplePaginate(5);
+        $param = ['items' => $items, 'sort' => $sort];
+        return view('hello.index', $param);
     }
 
     public function post(Request $request)
