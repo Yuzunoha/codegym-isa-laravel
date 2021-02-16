d84ae4cdebb1ef50d5e42f778ae19da1f24573b4
Yuzunoha <studysteadystandby@gmail.com>
2021-02-16 16:14:07 +0900

    migrateをじｋ

--- html/laravelapp/database/migrations/2021_02_16_071155_create_restdata_table.php
+++ html/laravelapp/database/migrations/2021_02_16_071155_create_restdata_table.php
@@ -6,24 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRestdataTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('restdata', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
+        Schema::create(
+            'restdata',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('message');
+                $table->string('url');
+                $table->timestamps();
+            }
+        );
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('restdata');
