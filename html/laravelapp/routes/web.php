ca16ed0267b7075379a7aa4528888ad43552bc73
Yuzunoha <studysteadystandby@gmail.com>
2021-02-18 14:36:29 +0900

    ログイン強制 ->middleware('auth')

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', 'HelloController@index')->middleware('auth');
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
